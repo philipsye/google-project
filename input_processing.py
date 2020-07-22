cb462b0488ce0dffc36402c2d7d9de4cca0a5049
philipsye <py@sem-wolf.org>
2020-07-22 22:21:08 +0300

    Further processing, deleting unrecognized characters

--- input_processing.py
+++ input_processing.py
@@ -1,3 +1,5 @@
+import chars_data
+
 def clearing_the_sentence(sentence):
     sentence = sentence.lower()
     sentence = sentence.replace(',', '')
@@ -5,7 +7,14 @@ def clearing_the_sentence(sentence):
     flag = False
 
     for i in range(len(sentence)):
-        if sentence[i-count] == ' ':
+        
+        if sentence[i-count] not in chars_data.chars.keys():
+            len_centence = len(sentence)
+            sentence = sentence.replace(sentence[i-count], '')
+            num = len_centence - len(sentence)
+            count += num
+
+        elif sentence[i-count] == ' ':
             if flag:
                 sentence = sentence[:i-count]+sentence[i-count+1:]
                 count += 1
