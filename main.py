7e8df51b40a91e44e05d029ffcce43188b64d554
philipsye <py@sem-wolf.org>
2020-07-22 23:24:52 +0300

    Insert multiple files into trie

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 import trie
 import time
 import input_processing
+import os
+import sys
+import ID_sentence
+
+ID = 0
+ID_sentences = list()
+
+def insert_data(root, file_name):
+    global ID
+
+    with open(file_name, encoding="utf8") as fp:
+        line = fp.readline()
+        cnt = 1
+       
+        while line:
+            print(cnt)
+            ID_sentences.append(ID_sentence.ID_sentence(file_name, cnt))
+            sentence =  line.strip()
+            for i in range(len(sentence)):
+                root.insert(input_processing.clearing_the_sentence(sentence), i, ID)
+            
+            ID += 1
+            #print("Line {}: {}".format(cnt, line.strip()))
+            line = fp.readline()
+            cnt += 1
+
 
 
 def main(): 
-  
-    # Input keys (use only 'a' through 'z' and lower case) 
-    setences = ["It *is* possible in some cases to change an object's type under certain controlled conditions. It generally isn't a good idea though since it can lead to some very strange behaviour if it is handled incorrectly",
-                "The \"hash()\" \"iter()\" \"reversed()\" and \"contains()\" methods have special handling for this; others will still raise a \"TypeError\" but may do so by relying on the behavior that \"None\" is not callable.",
-                "The \"hash()\" \"iter()\" \"reversed()\"",
-                "The \"hash()\" \"iter()\" \"reversed()\" and \"contain()\" methods have special handling for this; others will still raise a \"TypeError\" callable."
-    ] 
 
     t = trie.Trie() 
-    for setence in setences: 
-        for i in range(len(setence)):
-            t.insert(input_processing.clearing_the_sentence(setence), i)
+#   for setence in setences2: 
+#       for i in range(len(setence)):
+#           t.insert(input_processing.clearing_the_sentence(setence), i)
+#
+#   t1 = time.time()
+#   URL = t.search(input_processing.clearing_the_sentence("go"))
+#   for url in URL:
+#       if url:
+#           print(url)
+#   print(time.time()-t1)
 
-    t1 = time.time()
-    URL = t.search(input_processing.clearing_the_sentence("hash()\" \""))
-    for url in URL:
-        if url:
-            print(url)
-    print(time.time()-t1)
-
+#    insert_data(t, "perl-all-en-5.8.5.txt")
+    insert_data(t, "abstract.txt")
+    insert_data(t, "allocation.txt")
+    insert_data(t, "apiabiversion.txt")
+    insert_data(t, "arg.txt")
+    insert_data(t, "bool.txt")
+    insert_data(t, "buffer.txt")
+    insert_data(t, "bytearray.txt")
+    insert_data(t, "bytes.txt")
 
 if __name__ == '__main__': 
     main() 
